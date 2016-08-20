dcd80f451a56f6b0dc80f137844efca02acc2cac
zhengfy1 <zhengfy1@lenovo.com>
2016-08-20 19:10:02 +0800

    update

--- src/main/java/com/simple/common/util/ImageHandleUtil.java
+++ src/main/java/com/simple/common/util/ImageHandleUtil.java
@@ -1,6 +1,8 @@
 package com.simple.common.util;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
@@ -11,10 +13,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.ImageOutputStream;
+
+import com.sun.imageio.plugins.jpeg.*;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.Element;
 
 
 /**
@@ -120,98 +130,176 @@ public class ImageHandleUtil {
 	        return null;
 	}
 	
-	/**
-	 * 
-	 * @param imgInputStream
-	 * @param imgOutputStream
-	 * @param level
-	 */
-//	public static void scaleImage(InputStream imgInputStream, OutputStream imgOutputStream, int limit) {
-//		try {
-//			float per = 0.7f;
-//			Image src = javax.imageio.ImageIO.read(imgInputStream);
-//			int width = src.getWidth(null);
-//			int height = src.getHeight(null);
-//			double rate = 0;
-//			if(height > width){//如果高大于宽
-//				rate = height * 1.0 / limit;
-//				height = limit;//设置高为限制值
-//				width = (int) Math.floor(width / rate);
-//			} else {
-//				rate = width * 1.0 / limit;
-//				width = limit;//设置宽为限制值
-//				height = (int) Math.floor(height / rate);
-//			}
-//			
-////            // 构造Image对象  
-////            int old_w = src.getWidth(null); // 得到源图宽  
-////            int old_h = src.getHeight(null);  
-////            int new_w = 0;  
-////            int new_h = 0; // 得到源图长  
-////            double w2 = (old_w * 1.00) / (w * 1.00);  
-////            double h2 = (old_h * 1.00) / (h * 1.00);  
-//            // 图片跟据长宽留白，成一个正方形图。  
-//            BufferedImage oldpic = new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB); ;  
-////            if (old_w > old_h) {  
-////                oldpic = new BufferedImage(old_w, old_w,  
-////                        BufferedImage.TYPE_INT_RGB);  
-////            } else {  
-////                if (old_w < old_h) {  
-////                    oldpic = new BufferedImage(old_h, old_h,  
-////                            BufferedImage.TYPE_INT_RGB);  
-////                } else {  
-////                    oldpic = new BufferedImage(old_w, old_h,  
-////                            BufferedImage.TYPE_INT_RGB);  
-////                }  
-////            }  
-//            Graphics2D g = oldpic.createGraphics();  
-//            g.setColor(Color.white);  
-////            if (old_w > old_h) {  
-////                g.fillRect(0, 0, old_w, old_w);  
-////                g.drawImage(src, 0, (old_w - old_h) / 2, old_w, old_h,  
-////                        Color.white, null);  
-////            } else {  
-////                if (old_w < old_h) {  
-////                    g.fillRect(0, 0, old_h, old_h);  
-////                    g.drawImage(src, (old_h - old_w) / 2, 0, old_w, old_h,  
-////                            Color.white, null);  
-////                } else {  
-////  
-////                    // g.fillRect(0,0,old_h,old_h);  
-////                    g.drawImage(src.getScaledInstance(old_w, old_h,  
-////                            Image.SCALE_SMOOTH), 0, 0, null);  
-////                }  
-////            }  
-//            g.drawImage(src, 0, 0, width, height, Color.white, null);  
-//            g.dispose();  
-//            src = oldpic;  
-//            // 图片调整为方形结束  
-////            if (old_w > w)  
-////                new_w = (int) Math.round(old_w / w2);  
-////            else  
-////                new_w = old_w;  
-////            if (old_h > h)  
-////                new_h = (int) Math.round(old_h / h2);// 计算新图长宽  
-////            else  
-////                new_h = old_h;  
-//            BufferedImage tag = new BufferedImage(width, height,  
-//                    BufferedImage.TYPE_INT_RGB);  
-//            // tag.getGraphics().drawImage(src,0,0,new_w,new_h,null);  
-//            // 绘制缩小后的图  
-//            tag.getGraphics().drawImage(  
-//                    src.getScaledInstance(width, height, Image.SCALE_SMOOTH), 0,  
-//                    0, null);  
-//            JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(imgOutputStream);  
-//            JPEGEncodeParam jep = JPEGCodec.getDefaultJPEGEncodeParam(tag);  
-//            /* 压缩质量 */  
-//            jep.setQuality(per, true);  
-//            encoder.encode(tag, jep);  
-//            // encoder.encode(tag);  
-//            // 近JPEG编码  
-//            imgOutputStream.close();  
-//
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public static void main(String[] args) {
+		//img_change("D:\\","3.png");
+		File f = new File("D:\\3.png");
+		img_change(f,50);
+	}
+	
+	public static void img_change(File file,int with) {
+		Tosmallerpic(file,"_"+with,with,with,(float)0.7);
+	}
+	
+	public static String getScaleFilePath(String oriFilePath,int width) {
+		if (oriFilePath.contains(".")) {
+			return oriFilePath.substring(0,oriFilePath.lastIndexOf("."))+"_"+width+"."+oriFilePath.substring(oriFilePath.lastIndexOf(".")+1);
+		}
+		return oriFilePath;
+	}
+	
+	private static void img_change(String url,String name) {
+		Tosmallerpic(new File(url+name), "_middle", 188, 165, (float)0.7);
+		Tosmallerpic(new File(url+name), "_small", 45, 45, (float)0.7);
+		Tosmallerpic(new File(url+name), "_smaller", 116, 100, (float)0.7);
+	}
+	
+	/** 
+	    * 
+	    * @param f 图片所在的文件夹路径 
+	    * @param file 图片路径 
+	    * @param ext 扩展名 
+	    * @param n 图片名 
+	    * @param w 目标宽 
+	    * @param h 目标高 
+	    * @param per 百分比 
+	    */ 
+	    private static void  Tosmallerpic(File file,String ext,int w,int h,float per){ 
+	            Image src; 
+	            try { 
+	               src  =  javax.imageio.ImageIO.read(file); //构造Image对象 
+	               String fileAbsolutePath = file.getAbsolutePath();
+	               String img_midname  =  fileAbsolutePath.substring(0,fileAbsolutePath.lastIndexOf("."))+ext+"."+fileAbsolutePath.substring(fileAbsolutePath.lastIndexOf(".")+1); 
+	               int old_w = src.getWidth(null); //得到源图宽 
+	               int old_h = src.getHeight(null); 
+	               int new_w = 0; 
+	               int new_h = 0; //得到源图长 
+	 
+	               double w2 = (old_w*1.00)/(w*1.00); 
+	               double h2 = (old_h*1.00)/(h*1.00); 
+	 
+	               //图片跟据长宽留白，成一个正方形图。 
+	               BufferedImage oldpic; 
+	               if(old_w>old_h) 
+	               { 
+	                   oldpic = new BufferedImage(old_w,old_w,BufferedImage.TYPE_INT_RGB); 
+	               }else{if(old_w<old_h){ 
+	                   oldpic = new BufferedImage(old_h,old_h,BufferedImage.TYPE_INT_RGB); 
+	               }else{ 
+	                    oldpic = new BufferedImage(old_w,old_h,BufferedImage.TYPE_INT_RGB); 
+	               } 
+	               } 
+	                Graphics2D g  =  oldpic.createGraphics(); 
+	                g.setColor(Color.white); 
+	                if(old_w>old_h) 
+	                { 
+	                    g.fillRect(0, 0, old_w, old_w); 
+	                    g.drawImage(src, 0, (old_w - old_h) / 2, old_w, old_h, Color.white, null); 
+	                }else{ 
+	                    if(old_w<old_h){ 
+	                    g.fillRect(0,0,old_h,old_h); 
+	                    g.drawImage(src, (old_h - old_w) / 2, 0, old_w, old_h, Color.white, null); 
+	                    }else{ 
+	                        //g.fillRect(0,0,old_h,old_h); 
+	                        g.drawImage(src.getScaledInstance(old_w, old_h,  Image.SCALE_SMOOTH), 0,0,null); 
+	                    } 
+	                }              
+	                g.dispose(); 
+	                src  =  oldpic; 
+	                //图片调整为方形结束 
+	               /**图片大小判断暂时去掉
+	               if(old_w>w) 
+	               new_w = (int)Math.round(old_w/w2); 
+	               else 
+	                   new_w = old_w; 
+	               if(old_h>h) 
+	               new_h = (int)Math.round(old_h/h2);//计算新图长宽 
+	               else 
+	                   new_h = old_h; 
+	                   */
+	                new_w = w;
+	                new_h = h;
+	               BufferedImage image_to_save  =  new BufferedImage(new_w,new_h,BufferedImage.TYPE_INT_RGB);        
+	               image_to_save.getGraphics().drawImage(src.getScaledInstance(new_w, new_h,  Image.SCALE_SMOOTH), 0,0,null); 
+	               FileOutputStream fos = new FileOutputStream(img_midname); //输出到文件流 
+	                
+	               //旧的使用 jpeg classes进行处理的方法
+//	               JPEGImageEncoder encoder  =  JPEGCodec.createJPEGEncoder(fos); 
+//	               JPEGEncodeParam jep = JPEGCodec.getDefaultJPEGEncodeParam(image_to_save); 
+	                /* 压缩质量 */ 
+//	               jep.setQuality(per, true); 
+//	               encoder.encode(image_to_save, jep); 
+	                
+	               //新的方法
+	               saveAsJPEG(100, image_to_save, per, fos);
+	                
+	               fos.close(); 
+	               } catch (IOException ex) { 
+	            	   ex.printStackTrace();
+	                //Logger.getLogger(Img_Middle.class.getName()).log(Level.SEVERE, null, ex); 
+	            } 
+	    } 
+	     
+	   
+	    /**
+	     * 以JPEG编码保存图片
+	     * @param dpi  分辨率
+	     * @param image_to_save  要处理的图像图片
+	     * @param JPEGcompression  压缩比
+	     * @param fos 文件输出流
+	     * @throws IOException
+	     */
+	    public static void saveAsJPEG(Integer dpi ,BufferedImage image_to_save, float JPEGcompression, FileOutputStream fos) throws IOException {
+	          
+	        //useful documentation at http://docs.oracle.com/javase/7/docs/api/javax/imageio/metadata/doc-files/jpeg_metadata.html
+	        //useful example program at http://johnbokma.com/java/obtaining-image-metadata.html to output JPEG data
+	      
+	        //old jpeg class
+	        //com.sun.image.codec.jpeg.JPEGImageEncoder jpegEncoder  =  com.sun.image.codec.jpeg.JPEGCodec.createJPEGEncoder(fos);
+	        //com.sun.image.codec.jpeg.JPEGEncodeParam jpegEncodeParam  =  jpegEncoder.getDefaultJPEGEncodeParam(image_to_save);
+	      
+	        // Image writer
+	        JPEGImageWriter imageWriter  =  (JPEGImageWriter) ImageIO.getImageWritersBySuffix("jpg").next();
+	        ImageOutputStream ios  =  ImageIO.createImageOutputStream(fos);
+	        imageWriter.setOutput(ios);
+	        //and metadata
+	        IIOMetadata imageMetaData  =  imageWriter.getDefaultImageMetadata(new ImageTypeSpecifier(image_to_save), null);
+	         
+	         
+	        if(dpi !=  null && !dpi.equals("")){
+	             
+	             //old metadata
+	            //jpegEncodeParam.setDensityUnit(com.sun.image.codec.jpeg.JPEGEncodeParam.DENSITY_UNIT_DOTS_INCH);
+	            //jpegEncodeParam.setXDensity(dpi);
+	            //jpegEncodeParam.setYDensity(dpi);
+	      
+	            //new metadata
+	            Element tree  =  (Element) imageMetaData.getAsTree("javax_imageio_jpeg_image_1.0");
+	            Element jfif  =  (Element)tree.getElementsByTagName("app0JFIF").item(0);
+	            jfif.setAttribute("Xdensity", Integer.toString(dpi) );
+	            jfif.setAttribute("Ydensity", Integer.toString(dpi));
+	             
+	        }
+	      
+	      
+	        if(JPEGcompression >= 0 && JPEGcompression <= 1f){
+	      
+	            //old compression
+	            //jpegEncodeParam.setQuality(JPEGcompression,false);
+	      
+	            // new Compression
+	            JPEGImageWriteParam jpegParams  =  (JPEGImageWriteParam) imageWriter.getDefaultWriteParam();
+	            jpegParams.setCompressionMode(JPEGImageWriteParam.MODE_EXPLICIT);
+	            jpegParams.setCompressionQuality(JPEGcompression);
+	      
+	        }
+	      
+	        //old write and clean
+	        //jpegEncoder.encode(image_to_save, jpegEncodeParam);
+	      
+	        //new Write and clean up
+	        imageWriter.write(imageMetaData, new IIOImage(image_to_save, null, null), null);
+	        ios.close();
+	        imageWriter.dispose();
+	      
+	    }
 }
